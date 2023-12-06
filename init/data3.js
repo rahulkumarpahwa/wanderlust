a53025088ba360b062ae4791f9704caf9318c0c1
Rahul Kumar <rahulkumarpahwa@gmail.com>
2023-12-06 13:21:50 +0530

    Added Listings, Add Social Links

--- init/data3.js
+++ init/data3.js
@@ -21,7 +21,7 @@ const sampleListings = [
       url: "https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
     },
     price: 1200,
-    location: "New York City",
+    location: "New York",
     country: "United States",
     geometry: { type: "Point", coordinates: [75.818982, 26.915458] },
   },
--- init/index2.js
+++ init/index2.js
@@ -1,19 +1,21 @@
 //initialisation of sample listings
 
 const mongoose = require("mongoose");
-const ENV  = require ("../.env");
+// const ENV  = require ("../.env");
 const Listing = require("../models/listing.js"); //requiring model
 
 const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
-// const mapToken = `pk.eyJ1IjoiZGVsdGEtc3R1ZHVlbnQiLCJhIjoiY2xvMDk0MTVhMTJ3ZDJrcGR5ZDFkaHl4ciJ9.Gj2VU1wvxc7rFVt5E4KLOQ`;
-const mapToken = ENV.MAP_TOKEN;
+const mapToken = `pk.eyJ1IjoiZGVsdGEtc3R1ZHVlbnQiLCJhIjoiY2xvMDk0MTVhMTJ3ZDJrcGR5ZDFkaHl4ciJ9.Gj2VU1wvxc7rFVt5E4KLOQ`;
+// const mapToken = ENV.MAP_TOKEN;
 const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 // const initData = require("./data.js"); //requiring sample listings
 // const initData = require("./data2.js"); //requiring sample listings 2
 const initData = require("./data3.js"); //requiring sample listings 3
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"; //wanderlust is the name of our new database for major project.
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"; //wanderlust is the name of our new database for major project.
+const MONGO_URL = `mongodb+srv://thewittyedge:aHrRZYlExzAdsPjs@cluster0.qvciyqt.mongodb.net/?retryWrites=true&w=majority`; //wanderlust is the name of our new database for major project.
+
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
@@ -43,7 +45,7 @@ const initDB = async () => {
 
   initData.data = initData.data.map((obj) => ({
     ...obj,
-    owner: "6550d816625056e3e2b56a38",
+    owner: "656dd5a77406cdb9921a9d0d", //new demo
   })); //adding the owner with its object id.
   //here obj represents single listing.
 
